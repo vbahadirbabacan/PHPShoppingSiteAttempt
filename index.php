6103adc3ee863f1aa2bbc8503e9b6adc4b112388
basarckmclgl <basarcekmecelioglu6@gmail.com>
2022-12-11 19:52:23 +0300

    kategori yapıldı

--- index.php
+++ index.php
@@ -1,11 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+﻿<!DOCTYPE HTML>
+<html>
+<tr>
 <body>
-    
+<table border="1">
+<tr>
+<td>
+<h2>KATEGORİYE GÖRE ALIŞVERİŞ YAP</h2><hr>
+ <ul style="list-style-type:none;">
+    <li><a href="index1.php">AYAKKABI</a></li><hr>
+    <li><a href="index1.php">BAHÇE</a></li><hr>
+    <li><a href="index1.php">BEBEK</a></li><hr>
+    <li><a href="index1.php">BİLGİSAYAR</a></li><hr>
+    <li><a href="index1.php">ELEKTRONİK</a></li><hr>
+    <li><a href="index1.php">EV VE YAŞAM</a></li><hr>
+    <li><a href="index1.php">EVCİL HAYVAN</a></li><hr>
+    <li><a href="index1.php">GIDA ÜRÜNLERİ/a></li><hr>
+    <li><a href="index1.php">KİŞİSEL BAKIM</a></li><hr>  
+    <li><a href="index1.php">KOZMETİK</a></li><hr>
+    <li><a href="index1.php">KİTAP</a></li><hr>
+    <li><a href="index1.php">KADIN MODA</a></li><hr>
+    <li><a href="index1.php">ERKEK MODA</a></li><hr>
+    <li><a href="index1.php">ÇOCUK VE BEBEK MODA</a></li><hr>
+    <li><a href="index1.php">MUTFAK</a></li><hr>
+    <li><a href="index1.php">MÜZİK EKİPMANLARI VE ENSTRÜMANLARI</a></li><hr>
+    <li><a href="index1.php">OFİS VE KIRTASİYE</a></li><hr>
+    <li><a href="index1.php">OTOMOTİV</a></li><hr>
+    <li><a href="index1.php">OYUNCAK</a></li><hr>
+    <li><a href="index1.php">SAĞLIK, BAKIM, TEMİZLİK</a></li><hr>
+    <li><a href="index1.php">SPOR VE OUTDOOR</a></li><hr>
+    <li><a href="index1.php">TELEFON</a></li><hr>
+    <li><a href="index1.php">TELEVİZYONLAR VE EV SİNEMA SİSTEMLERİ</a></li><hr>
+    <li><a href="index1.php">VİDEO VE KONTROL</a></li><hr>
+    <li><a href="index1.php">YAPI MARKET</a></li><hr>
+    <li><a href="index1.php">OUTLET REYONU</a></li><hr>
+ </ul>
+ </td>
+ </tr>
+ </table>
 </body>
 </html>
---
+++ index1.php
@@ -0,0 +1,8 @@
+﻿<!DOCTYPE HTML>
+<html>
+<body>
+<table border="1" align="center" width="800" height="900">
+</table>
+</body>
+</html>
+
